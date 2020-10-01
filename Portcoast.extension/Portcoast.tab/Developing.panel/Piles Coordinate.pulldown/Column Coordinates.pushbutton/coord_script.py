b7fbdccec86414c6182e8140c87d4a243c361e4e
nhantruong96 <nhan96bk@gmail.com>
2020-10-01 18:48:36 +0700

    Update coord_script.py

--- Portcoast.extension/Portcoast.tab/Developing.panel/Piles Coordinate.pulldown/Column Coordinates.pushbutton/coord_script.py
+++ Portcoast.extension/Portcoast.tab/Developing.panel/Piles Coordinate.pulldown/Column Coordinates.pushbutton/coord_script.py
@@ -17,32 +17,10 @@ try:
     doc = uidoc.Document
     app = __revit__.Application
     
-    selected_ElementIDs = uidoc.Selection.GetElementIds()
-    selected_Elements = map(lambda x: doc.GetElement(x), selected_ElementIDs)
+    selected_Elements = map(lambda x: doc.GetElement(x), uidoc.Selection.GetElementIds())
     
     # Filter to get Structural Columns
     piles_list = list(filter(lambda x: x.Category.Name == "Structural Columns", selected_Elements))
-    points_list = []
-    angle_list = []
-    x_pile_local_list = []
-    y_pile_local_list = []
-    for i in piles_list:
-        location = i.Location
-        location_point = i.Location.Point
-        
-        # Points List
-        points_list.append(location_point)
-        
-        # Angles List
-        y = location_point.Y
-        y_pile_local_list.append(y)
-        x = location_point.X
-        x = x_pile_local_list.append(x)
-        angle = i.Location.Point.AngleTo(XYZ(1,0,0))
-        if y>0:
-            angle_list.append(angle)
-        else:
-            angle_list.append(2*math.pi-angle)
     
     # Get Project Base Point
     project_base_point = FilteredElementCollector(doc).OfCategory(OST_ProjectBasePoint).ToElements()[0]
@@ -52,42 +30,34 @@ try:
     y_survey_global = shared_base_point.GetParameters("N/S")[0].AsDouble()
     x_survey_global = shared_base_point.GetParameters("E/W")[0].AsDouble()
     vec_survey_global = XYZ(x_survey_global, y_survey_global, 0)
-    survey_vec = shared_base_point.Position
-    survey_vec_negate = survey_vec.Negate()
-    y_survey_local_negate = survey_vec_negate.Y
-    x_survey_local_negate = survey_vec_negate.X
-    
-    # Get Lengths
-    lengths = map(lambda x: x.GetLength(), points_list)
-
-    # New Angles List
-    new_angles_list = map(lambda a1 : a1 - a0 +(a0//math.pi)*2*math.pi, angle_list)
+    survey_vec_negate = shared_base_point.Position.Negate()
 
     # New Coords
-    x_list = []
-    y_list = []
-    for i in range(len(points_list)):
-        x = x_pile_local_list[i] + (x_survey_local_negate + x_survey_global)
-        y = y_pile_local_list[i] + (y_survey_local_negate + y_survey_global)
-        x_list.append(x)
-        y_list.append(y)
+    vec_pile_global = []
+    for i in piles_list:
+        vec_StoP = i.Location.Point.Add(survey_vec_negate)
+        x_StoP = vec_StoP.X
+        y_StoP = vec_StoP.Y
+        rotated_vec_StoP = XYZ(x_StoP * math.cos(a0) - y_StoP * math.sin(a0), x_StoP * math.sin(a0) + y_StoP * math.cos(a0), 0)
+        vec = rotated_vec_StoP.Add(vec_survey_global)
+        vec_pile_global.append(vec)    
         
     x_param_list = map(lambda e: e.LookupParameter("X"), piles_list)
     y_param_list = map(lambda e: e.LookupParameter("Y"), piles_list)
     
     trans = Transaction(doc, "Set X Value")
     trans.Start()
-    for px, py, x, y in zip(x_param_list, y_param_list, x_list, y_list):
-        pxOk = px.Set(x)
-        pxOk = py.Set(y)
+    pxOk_list = []
+    pyOk_list = []
+    for px, py, vec in zip(x_param_list, y_param_list, vec_pile_global):
+        pxOk = px.Set(vec.X)
+        pyOk = py.Set(vec.Y)
+        pxOk_list.append(pxOk)
+        pyOk_list.append(pyOk)
+        if not pxOk or not pxOk:
+            print(px, py)
     trans.Commit()
-    
-    location_pile_local = []
-    for i in piles_list:
-        
-        vec_survey_to_pile = i.Location.Point.Add(survey_vec_negate)
-        rotated_vec_survey_to_pile = XYZ(vec_survey_to_pile.X * math.cos(a0)-vec_survey_to_pile.Y * math.sin(a0), vec_survey_to_pile.X * math.sin(a0) + vec_survey_to_pile.Y * math.cos(a0), 0)
-        vec = rotated_vec_survey_to_pile.Add(vec_survey_global)
-        print(vec.Multiply(304.8))
+    if not pxOk_list or not pyOk_list:
+        TaskDialog.Show("Succeed","Mission's completed!")
 except Exception as error:
     TaskDialog.Show("Error",str(error))
